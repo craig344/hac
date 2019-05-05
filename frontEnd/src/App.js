6f811b1b29ece22c56ff3e79d2e060b942da6ff9
anishrao16 <rao.anish63@gmail.com>
2019-05-05 14:01:49 +0530

    test

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter} from "react-router-dom";
-
-import Content from './components/Content';
-import NavBar from './components/navBar';
+//import { BrowserRouter} from "react-router-dom";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import ExportToExcel from './components/ExportToExcel';
+//import Content from './components/Content';
+//import NavBar from './components/navBar';
 
 class App extends Component {
 
